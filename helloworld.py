fde5f74d8f18aa253be7c4a8dc6a17e25ec9f928
Sameer Ansari <elucidation@gmail.com>
2011-05-01 22:13:38 -0400

    Added names and dead checking

--- helloworld.py
+++ helloworld.py
@@ -6,16 +6,31 @@ class Environment:
         self.MAXWIDTH = 100
         self.MAXHEIGHT = 100
         self.creatures = []
+    def step(self):
+        for c in self.creatures:
+            c.energy -= 1
+        self.removeDead()
+    def removeDead(self):
+        for c in self.creatures:
+            print "Checking if ",c," is dead... ",c.isDead()
 
 class Creature:
     "It lives..."
-    def __init__(self):
+    def __init__(self,name):
+        self.name = name
         self.x = 0
         self.y = 0
         self.energy = 100
+    def isDead(self):
+        return self.energy <= 0
+    def __str__(self):
+        return self.name
         
 World = Environment()
 
-bubs = Creature()
+bubs = Creature("Bubs")
 
 World.creatures.append(bubs)
+
+
+World.step()
