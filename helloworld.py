1c425e35720772bf2c7744b4ad1e514b6536a32a
Sameer Ansari <elucidation@gmail.com>
2011-05-01 22:06:40 -0400

    First world attempt, simple environment and one creature

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,21 @@
 print "Hello World!"
 
+class Environment:
+    "Where Creatures live, 2D plane 100x100 units starting from 0,0"
+    def __init__(self):
+        self.MAXWIDTH = 100
+        self.MAXHEIGHT = 100
+        self.creatures = []
 
-for i in range(1,20):
-    print "Iterating #",i
-    
+class Creature:
+    "It lives..."
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.energy = 100
+        
+World = Environment()
+
+bubs = Creature()
+
+World.creatures.append(bubs)
